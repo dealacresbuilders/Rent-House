029d5ac44ff60aaca7e7ba46b72c2acc5d87a732
Mukesh Kumar <mk8701952@gmail.com>
2026-04-10 14:54:06 +0530

    dfghjk

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,7 @@ import { PropertyProvider } from "@/contextapi/propertycontext"; // ✅ ADD THIS
 import Footer from "@/components/Footer";
 import ScrollToTop from "@/components/ScrollToTop";
 import { BlogProvider } from "@/contextapi/BlogContext";
+import Script from "next/script";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -16,26 +17,100 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Rent House in Faridabad",
-  description: "Find verified houses for rent in Faridabad with best deals.",
-};
+title: "Rent House in Faridabad | Affordable & Luxury Rental Homes",
 
+description:
+"Find verified houses for rent in Faridabad including independent houses, villas, and builder floors at affordable prices. Explore ready to move rental homes in prime locations of Faridabad.",
+
+keywords: [
+  "rent house faridabad",
+  "house on rent faridabad",
+  "homes for rent faridabad",
+  "independent house rent faridabad",
+  "villa on rent faridabad",
+  "builder floor rent faridabad",
+  "rental property faridabad",
+  "cheap house rent faridabad",
+  "luxury house rent faridabad",
+  "residential rent faridabad"
+],
+
+  alternates: {
+    canonical: "www.houseforrentinfaridabad.com/",
+  },
+  verification: {
+    google: "E5Cv_41W3Z16pv3hqk-twbghqT0qgR7btFlmd-zcOWU",
+  },
+   icons: {
+      icon: "/favicon.ico",
+      shortcut: "/favicon.ico",
+      apple: "/favicon.ico",
+    },
+};
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable}`}
+    >
+      <head>
+        {/* ✅ Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-KJ23TVKZ');
+            `,
+          }}
+        />
+
+        {/* ✅ Google Analytics (GA4) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-XP5YLVK2EE"
+          strategy="afterInteractive"
+        />
+        <Script id="ga-script" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-XP5YLVK2EE');
+          `}
+        </Script>
+      </head>
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {/* ✅ Provider Wrap Start */}
+        {/* ✅ GTM NoScript */}
+        
+ <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KJ23TVKZ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
+
+        {/* ✅ Providers */}
         <PropertyProvider>
           <BlogProvider>
-          <Navbar />
-          {children}
-          <ScrollToTop />
-          <Footer/>
+            <Navbar />
+            {children}
+            <ScrollToTop />
+            <Footer />
+            <Toaster position="top-right" reverseOrder={false} />
           </BlogProvider>
         </PropertyProvider>
-        {/* ✅ Provider Wrap End */}
       </body>
     </html>
   );

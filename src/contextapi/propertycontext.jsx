6efdd7ea076b876ccf659ed20d16e680c4f7a350
Mukesh Kumar <mk8701952@gmail.com>
2026-06-23 12:12:18 +0530

    auto update

--- src/contextapi/propertycontext.jsx
+++ src/contextapi/propertycontext.jsx
@@ -77,8 +77,19 @@ const fetchPropertiesByType = async () => {
     setError3(null);
 
     const res = await axios.get(
-      `https://faridabad-backend.onrender.com/api/listed-properties/getPropertiesByType/${type}/${domain}?page=${page}`
+      `https://faridabad-backend.onrender.com/api/listed-properties/properties`,
+      {
+        params: {
+          listingType: "rent",
+          propertyType: type,
+          subType: "House,Bedroom",
+          city: "Faridabad",
+          page,
+          limit: 150,
+        },
+      }
     );
+ console.log("API Response", res.data);
 
     setData2(res.data?.data || []);
 

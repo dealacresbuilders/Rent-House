cad215efaa7c331f4b270728264ebe13d0b3b3f7
dealacresfaridabad <dealacressocialmedia@gmail.com>
2026-06-27 16:51:58 +0530

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from "react";
 import Link from "next/link";
-import { locations } from "../data/locations";
+import { locations } from "../data/newLocations";
 
 const createSlug = (location) => {
   return location
